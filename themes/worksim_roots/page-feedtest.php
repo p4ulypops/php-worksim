9dcfed1b3a3794389bf2deb312fcced480052258
Duncan Stuart <dgmstuart@gmail.com>
2014-10-24 16:07:26 +0100

    Shows posts from mulipule feeds

--- themes/worksim_roots/page-feedtest.php
+++ themes/worksim_roots/page-feedtest.php
@@ -26,6 +26,44 @@ if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
     $rss_items = $rss->get_items( 0, $maxitems );
 
 endif;
+
+
+$feeds = [
+    'http://swungover.wordpress.com/feed/',
+    'http://awordonswing.wordpress.com/feed/',
+    'http://lindyaffair.wordpress.com/feed/'];
+
+foreach($feeds as $feed) {
+    $currentFeed = fetch_feed($feed);
+    if (! is_wp_error($currentFeed)) {
+        $maxitems = $currentFeed->get_item_quantity(5);
+        $current_rss_items = $currentFeed->get_items(0, $maxitems);
+    }
+
+    foreach($current_rss_items as $item) {
+        $finalArray[] = [
+            'title'=> $item->get_title(),
+            'content' => $item->get_content(),
+            'date' => strtotime($item->get_date())
+        ];
+    }
+
+
+
+}
+
+
+usort($finalArray, function($a, $b) {
+    return ($a['date'] > $b['date'] ? -1 : 1);
+});
+
+Whippet::print_r(array_map(function($x) {
+    return $x['date'];
+}, $finalArray ));
+
+
+
+
 ?>
 
 
@@ -33,16 +71,16 @@ endif;
         <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
     <?php else : ?>
         <?php // Loop through each feed item and display  each item as a hyperlink. ?>
-        <?php foreach ( $rss_items as $item ) : ?>
+        <?php foreach ( $finalArray as $item ) : ?>
             <article>
-                <header><h1><?=$item->get_title();?></h1></header>
-
-                <?= $item->get_content(); ?>
+                <header><h1><?=$item['title'];?></h1></header>
+                <?= $item['content'] ?>
+                <footer><?= date("Y m d ", $item['date']) ?></footer>
             </article>
 
         <?php endforeach; ?>
     <?php endif; ?>
 
-
+?>
 
 <? wp_foot(); ?>
