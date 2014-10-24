304fc20a7999ac9f773fbb3ce0882a2dbbdf3132
Duncan Stuart <dgmstuart@gmail.com>
2014-10-24 15:43:33 +0100

    Page feed - single feed

--- themes/worksim_roots/page-feedtest.php
+++ themes/worksim_roots/page-feedtest.php
@@ -20,7 +20,7 @@ $rss = fetch_feed( 'http://swungover.wordpress.com/feed/' );
 if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 
     // Figure out how many total items there are, but limit it to 5.
-    $maxitems = $rss->get_item_quantity( 1 );
+    $maxitems = $rss->get_item_quantity( 4 );
 
     // Build an array of all the items, starting with element 0 (first element).
     $rss_items = $rss->get_items( 0, $maxitems );
@@ -28,21 +28,21 @@ if ( ! is_wp_error( $rss ) ) : // Checks that the object is created correctly
 endif;
 ?>
 
-<ul>
+
     <?php if ( $maxitems == 0 ) : ?>
         <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
     <?php else : ?>
-        <?php // Loop through each feed item and display each item as a hyperlink. ?>
+        <?php // Loop through each feed item and display  each item as a hyperlink. ?>
         <?php foreach ( $rss_items as $item ) : ?>
-            <li>
-                <a href="<?php echo esc_url( $item->get_permalink() ); ?>"
-                    title="<?php printf( __( 'Posted %s', 'my-text-domain' ), $item->get_date('j F Y | g:i a') ); ?>">
-                    <?php echo esc_html( $item->get_title() ); ?>
-                </a>
-            </li>
+            <article>
+                <header><h1><?=$item->get_title();?></h1></header>
+
+                <?= $item->get_content(); ?>
+            </article>
+
         <?php endforeach; ?>
     <?php endif; ?>
-</ul>
+
 
 
 <? wp_foot(); ?>
