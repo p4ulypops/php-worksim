e360298d2536267d35d612c7c789c9feb31f4613
Paul Silver <me@paul.gd>
2014-10-26 13:54:12 +0000

    cleanup

--- themes/worksim_roots/page-feedtest.php
+++ themes/worksim_roots/page-feedtest.php
@@ -1,58 +1,48 @@
 <?php
 /*
-Template Name: Feed Test
+Template Name: Display Feeds
 */
 
 // Get RSS Feed(s)
-include_once( ABSPATH . WPINC . '/feed.php' );
+include_once(ABSPATH . WPINC . '/feed.php');
 
 wp_head();
 
 ?>
-<h2><?php _e( 'Recent news from Some-Other Blog:', 'my-text-domain' ); ?></h2>
+<h2><?php _e('Recent news from Some-Other Blog:', 'my-text-domain'); ?></h2>
 <?php
 
-/*
-$feeds = [
-    'http://swungover.wordpress.com/feed/',
-    'http://awordonswing.wordpress.com/feed/',
-    'http://lindyaffair.wordpress.com/feed/'];
-*/
-
-    // The Query
-    $the_query = new WP_Query( array('post_type' => array('blog'), 'publish' => 'Published' ) );
+// The Query
+$the_query = new WP_Query(array('post_type' => array('blog'), 'publish' => 'Published'));
 //echo '.........'.print_r($the_query,);
-    // The Loop
-    if ( $the_query->have_posts() ) {
-        //echo '<ul>';
-        while ( $the_query->have_posts() ) {
-            $the_query->the_post();
-            $feeds[] = get_post_meta(get_the_id(), 'url')[0];
+// The Loop
+if ($the_query->have_posts()) {
+    //echo '<ul>';
+    while ($the_query->have_posts()) {
+        $the_query->the_post();
+        $feeds[] = get_post_meta(get_the_id(), 'url')[0];
 
-        }
-        //echo '</ul>';
-    } else {
-        // no posts found
     }
-    /* Restore original Post Data */
-    wp_reset_postdata();
+}
+/* Restore original Post Data */
+wp_reset_postdata();
 
 
 //Whippet::print_r($feeds);
 //exit();
 
-foreach($feeds as $feed) {
+foreach ($feeds as $feed) {
 
     $currentFeed = fetch_feed($feed);
-    if (! is_wp_error($currentFeed)) {
+    if (!is_wp_error($currentFeed)) {
         $maxitems = $currentFeed->get_item_quantity(5);
         $current_rss_items = $currentFeed->get_items(0, $maxitems);
     }
 
-    foreach($current_rss_items as $item) {
+    foreach ($current_rss_items as $item) {
 
         $finalArray[] = [
-            'title'=> $item->get_title(),
+            'title' => $item->get_title(),
             'content' => $item->get_content(),
             'date' => strtotime($item->get_date()),
             'permalink' => $item->get_permalink(),
@@ -60,24 +50,24 @@ foreach($feeds as $feed) {
     }
 }
 
-usort($finalArray, function($a, $b) {
+usort($finalArray, function ($a, $b) {
     return ($a['date'] > $b['date'] ? -1 : 1);
 });
 ?>
 
-    <?php if ( $maxitems == 0 ) : ?>
-        <li><?php _e( 'No items', 'my-text-domain' ); ?></li>
-    <?php else : ?>
-        <?php // Loop through each feed item and display  each item as a hyperlink. ?>
-        <?php foreach ( $finalArray as $item ) : ?>
-            <article>
-                <header>
-                    <h1><?=$item['title'];?></h1>
-                    <p><a href='<?= $item['permalink']?>' target='_blank'>Read the Original Post</a></p>
-                </header>
-                <?= $item['content'] ?>
-            </article>
+<?php if ($maxitems == 0) : ?>
+    <li><?php _e('No items', 'my-text-domain'); ?></li>
+<?php else : ?>
+    <?php foreach ($finalArray as $item) : ?>
+        <article>
+            <header>
+                <h1><?= $item['title']; ?></h1>
 
-        <?php endforeach; ?>
-    <?php endif; ?>
+                <p><a href='<?= $item['permalink'] ?>' target='_blank'>Read the Original Post</a></p>
+            </header>
+            <?= $item['content'] ?>
+        </article>
+
+    <?php endforeach; ?>
+<?php endif; ?>
 <? wp_foot(); ?>
--- themes/worksim_roots/page-submitsite.php
+++ themes/worksim_roots/page-submitsite.php
@@ -8,70 +8,73 @@ wp_head();
 $message = [];
 
 if (isset($_POST['submit'])) {
-/*
-        if (
-            ! isset( $_POST['wp_nonce'] )
-            || ! wp_verify_nonce( $_POST['wp_nonce'], 'submit_blog' )
-        ) {
 
-          $message['general'] = "Sorry, something has gone wrong, please try again";
+    if (
+        !isset($_POST['wp_nonce'])
+        || !wp_verify_nonce($_POST['wp_nonce'], 'add-blog')
+    ) {
 
+        $message['general'] = "Sorry, something has gone wrong, please try again";
+
+    } else {
+
+        // Email address
+        if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $message['email'] = "Please provide valid email address";
+        }
+        if (!filter_var($_POST['url'], FILTER_VALIDATE_URL)) {
+            $message['url'] = "Please provide valid URL";
+        }
+        if (empty($_POST['personname'])) {
+            $message['personname'] = "Please let us know your name";
+        }
+
+
+        $my_post = array(
+            'post_title' => $_POST['personname'] . ' \'s blog',
+            'post_type' => 'blog',
+            'post_content' => '',
+            'post_status' => 'draft'
+        );
+
+        // Insert the post into the database
+        $post_id = wp_insert_post($my_post);
+
+        if (!is_wp_error($post_id)) {
+            update_post_meta($post_id, 'name', $_POST['name']);
+            update_post_meta($post_id, 'email', $_POST['email']);
+            update_post_meta($post_id, 'url', $_POST['url']);
+
+            $message['thanks'] = "Thank you for submitting your blog (" . $post_id . ")";
         } else {
-*/
-              // Email address
-              if (! filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
-                $message['email'] = "Please provide valid email address";
-              }
-              if (! filter_var($_POST['url'], FILTER_VALIDATE_URL) ) {
-                $message['url'] = "Please provide valid URL";
-              }
-              if (empty($_POST['personname']) ) {
-                $message['personname'] = "Please let us know your name";
-              }
+            $message[] = print_r($post_id, true);
+        }
 
-
-            $my_post = array(
-              'post_title'     => $_POST['personname'].' \'s blog',
-              'post_type'    => 'blog',
-              'post_content'  => 'This is my post.',
-              'post_status'   => 'draft'
-            );
-
-            // Insert the post into the database
-            $post_id = wp_insert_post( $my_post );
-
-            if (! is_wp_error($post_id)) {
-              update_post_meta($post_id, 'name', $_POST['name']);
-              update_post_meta($post_id, 'email', $_POST['email']);
-              update_post_meta($post_id, 'url', $_POST['url']);
-
-              $message['thanks'] = "Thank you for submitting your blog (".$post_id.")";
-            } else {
-              $message[] = print_r($post_id, true);
-            }
-
-   // }
+    }
 }
 ?>
 
 <h1>Submit a Blog</h1>
 
 <form action="" method="POST" class="submitform">
-  <label for="frmEmail">Your email address: <input type="email" id="frmEmail" name="email" required value="me@paul.gd"></label>
-  <label for="frmName">Your Name: <input type="text" id="frmName" name="personname" required value="John"></label>
-  <label for="frmURL">Your blog's RSS Feed: <input type="URL" id="frmURL" name="url" required value="http://swungover.wordpress.com/feed/"></label>
-  <input type="submit" name="submit" value="Submit your blog" />
-
-  <?php // if (isset($message) && count($mesage) > 0) { ?>
-      <ul>
-        <li>
-          <?= implode("</li><li>", $message) ?>
-        </li>
-      </ul>
-  <?php // } ?>
+    <label for="frmEmail">Your email address: <input type="email" id="frmEmail" name="email" required
+                                                     value="me@paul.gd"></label>
+    <label for="frmName">Your Name: <input type="text" id="frmName" name="personname" required value="John"></label>
+    <label for="frmURL">Your blog's RSS Feed: <input type="URL" id="frmURL" name="url" required
+                                                     value="http://swungover.wordpress.com/feed/"></label>
+    <? wp_nonce_field('add-blog', 'wp_nonce'); ?>
+    <input type="submit" name="submit" value="Submit your blog"/>
 
 </form>
+<?php if (isset($message) && count($mesage) > 0) { ?>
+    <ul>
+        <? foreach ($message as $m) { ?>
+            <li><?= $m ?></li>
+        <? } ?>
+    </ul>
+<?php } ?>
 
 
 
-<?php wp_foot();?>
+
+<?php wp_foot(); ?>
